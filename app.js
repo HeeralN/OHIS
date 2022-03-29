7500b8d86537f08499bbdeabb9a035e38e19c73e
Srija <srija.gottiparthi@gmail.com>
2022-03-28 21:47:26 -0400

    delete listings works + admin dashboard button on navbar

--- app.js
+++ app.js
@@ -116,14 +116,13 @@ app.get('/adminManagingUsers', function(req, res) {
     }
 });
 
-// THIS DOES NOT WORK YET
 app.post('/adminManagingListings/getListing', function(req, res) {
     const housingId = req.body.listingID;
+
     if (housingId) {
-        db.query('SELECT listingId, address, user email, date created, last modified, link, description FROM listing WHERE listingId = ?', [housingId], function(error, results, fields) {
+        db.query('SELECT listingId, address, username, date_created, last_modified, link, description FROM listing WHERE listingId = ?', [housingId], function(error, results, fields) {
             if (results.length > 0) {
-                // CHANGE THESE FIELD NAMES
-                return res.render('adminManagingListings', {listingID: results[0].listingId, address: results[0].address, email: results[0].user_email, date_created: results[0].date_created, last_modified: results[0].last_modified, link: results[0].link, description: results[0].description});
+                return res.render('adminManagingListings', {listingId: results[0].listingId, address: results[0].address, username: results[0].username, date_created: results[0].date_created, last_modified: results[0].last_modified, link: results[0].link, description: results[0].description});
             } else {
                 return res.render('adminManagingListings', {message: 'Listing not found!'});
             }
@@ -131,17 +130,16 @@ app.post('/adminManagingListings/getListing', function(req, res) {
         });
     }
     else {
-        return res.render('adminManagingListings', {message: 'Please enter a value!'});
+        return res.render('adminManagingListings', {message: 'Please enter a value!'}); 
     }
 });
 
-// DOES NOT WORK YET
-app.post('/adminManagingUsers/deleteListing', function(req, res) {
+app.post('/adminManagingListings/deleteListing', function(req, res) {
     const housingId = req.body.listingID;
+
     if (housingId) {
         db.query('DELETE FROM listing WHERE listingId = ?', [housingId], function(error, results, fields) {
             if (results && error === null) {
-                //res.send('User deleted!');
                 return res.render('adminManagingListings', {message: 'Listing deleted!'});
             } else {
                 return res.render('adminManagingListings', {message: 'There was an error deleting this listing! It may have been deleted already.'});
@@ -160,7 +158,6 @@ app.post('/adminManagingUsers/getUser', function(req, res) {
     if (userId) {
         db.query('SELECT username, email, fullname, adminPerms FROM account WHERE username = ? AND username != ?', [userId, req.session.username], function(error, results, fields) {
             if (results && results.length > 0) {
-                //res.send(results);
                 return res.render('adminManagingUsers', {username: results[0].username, email: results[0].email, fullname: results[0].fullname, adminPerms: results[0].adminPerms});
             } else {
                 return res.render('adminManagingUsers', {message: 'User not found!'});
